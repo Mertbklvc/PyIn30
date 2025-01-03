45a48ffa3b5b0ff48cf69219949aba199e290024
Mertbklvc <mertcanbklvc@gmail.com>
2025-01-03 16:58:57 +0100

    Day1 v.1

---
+++ Day1/Bandnamen_Generator.py
@@ -0,0 +1,4 @@
+print("Willkommen beim Bandnamen-Generator!")
+Geburtsort = input("Bitte geben Sie Ihren Geburtsort ein!\n")
+Haustier_name= input("Bitte geben Sie den Namen Ihres Haustiers ein!\n")
+print("Ihr Bandname könnte sein: " + Geburtsort + " " + Haustier_name)
---
+++ Day1/Day1.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+# Mit der Funktion print() können wir einen beliebigen String-Ausdruck auf der Konsole ausgeben.
+
+print("Hello World!\nHello World!\nHello World!")
+# Mit dem Befehl \n können wir innerhalb einer einzigen print()-Funktion Zeichenketten in mehreren Zeilen auf der Konsole ausgeben.
+
+print("Mert" + " " + "Bekleviç")
+# Mit der Funktion print() können wir verschiedene Zeichenketten mit + verbinden.
+
+input("Wie heißt du?")
+# Mit der Funktion input() können wir eine Eingabevariable vom Benutzer erhalten.
+
+print("Hello" + " " + input("Wie heißt du?") + "!")
+# Wir können einfach die Input-Funktion in unsere Print-Funktion integrieren.
+
+name = input("Was ist deine Lieblingsfarbe?")
+print("Deine Lieblingsfarbe ist " + name)
+# Wir können diesen Prozess auch mit Variablen durchführen.
+
+name = input("Welche Mannschaft unterstützt du?")
+length = len(name)
+print(length)
+# Wir können mit der len-Funktion bestimmen, wie viele Buchstaben die Variablen haben, die wir in die len-Funktion schreiben.
+
